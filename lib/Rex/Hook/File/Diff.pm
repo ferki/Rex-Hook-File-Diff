b5baa317c2742746efa3c1c8d654d1c6282663a0
Ferenc Erki <erkiferenc@gmail.com>
2020-04-23 22:26:58 +0200

    Add initial implementation

--- lib/Rex/Hook/File/Diff.pm
+++ lib/Rex/Hook/File/Diff.pm
@@ -1,12 +1,59 @@
 package Rex::Hook::File::Diff;
 
-# ABSTRACT:
+# ABSTRACT: show diff of changes for files managed by Rex
 
-use 5.010;
+use 5.012;
 use strict;
 use warnings;
 
 our $VERSION = '9999';
+
+use Rex 0.044 -base;
+use Rex::Hook;
+use Text::Diff 1.44;
+
+register_function_hooks { before_change => { file => \&show_diff, }, };
+
+sub show_diff {
+    my ( $original_file, @opts ) = @_;
+
+    my $diff = diff( involved_files($original_file) );
+
+    if ( length $diff > 0 ) { print "Diff for: $original_file\n$diff" }
+
+    return;
+}
+
+sub involved_files {
+    my $file = shift;
+
+    my $temp_file = get_rex_temp_file_path($file);
+    my $null      = File::Spec->devnull();
+
+    if ( !is_file($file) )      { $file      = $null }    # creating file
+    if ( !is_file($temp_file) ) { $temp_file = $null }    # removing file
+
+    return ( $file, $temp_file );
+}
+
+sub get_rex_temp_file_path {
+    my $file = shift;
+
+    ## no critic ( CodeLayout RegularExpressions ValuesAndExpressions )
+    # copied verbatim from Rex::Commands::File
+
+    my @splitted_file = split( /[\/\\]/, $file );
+    my $file_name     = ".rex.tmp." . pop(@splitted_file);
+    my $tmp_file_name = (
+        $#splitted_file != -1
+        ? ( join( "/", @splitted_file ) . "/" . $file_name )
+        : $file_name
+    );
+
+    ## use critic
+
+    return $tmp_file_name;
+}
 
 1;
 
@@ -19,6 +66,8 @@ __END__
     use Rex::Hook::File::Diff;
 
 =head1 DESCRIPTION
+
+This module allows L<Rex> to show a diff of changes for the files managed via its built-in L<file|https://metacpan.org/pod/Rex::Commands::File#file> command.
 
 =head1 DIAGNOSTICS
 
@@ -39,5 +88,7 @@ There are no known incompatibilities with other modules.
 =head1 BUGS AND LIMITATIONS
 
 There are no known bugs. Make sure they are reported.
+
+Currently the module supports local file operations only.
 
 =cut
