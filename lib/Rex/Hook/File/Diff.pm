c82c52bf8d5aabf12d018b6ce91a223578ba4537
Ferenc Erki <erkiferenc@gmail.com>
2020-04-23 20:39:37 +0200

    Initial commit

---
+++ lib/Rex/Hook/File/Diff.pm
@@ -0,0 +1,43 @@
+package Rex::Hook::File::Diff;
+
+# ABSTRACT:
+
+use 5.010;
+use strict;
+use warnings;
+
+our $VERSION = '9999';
+
+1;
+
+__END__
+
+=for :stopwords CPAN
+
+=head1 SYNOPSIS
+
+    use Rex::Hook::File::Diff;
+
+=head1 DESCRIPTION
+
+=head1 DIAGNOSTICS
+
+This module does not do any error checking (yet).
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+This module does not require any configuration, nor does it use any environment variables.
+
+=head1 DEPENDENCIES
+
+See L<cpanfile>.
+
+=head1 INCOMPATIBILITIES
+
+There are no known incompatibilities with other modules.
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs. Make sure they are reported.
+
+=cut
